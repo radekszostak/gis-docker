022c4ce15a8580cb80c13d25c2029dc12d09473f
Radoslaw Szostak <radoslawszostak@gmail.com>
2022-11-03 16:22:52 +0100

    compose

---
+++ gis-db.bat
@@ -0,0 +1,4 @@
+explorer "http://localhost:8888"
+set GIS_WORK_DIR=%cd%
+cd %~dp0
+docker-compose up --build
---
+++ gis.bat
@@ -0,0 +1,9 @@
+explorer "http://localhost:8888"
+
+docker run ^
+-v %cd%:/home/workdir ^
+-v %cd%/.docker/.vscode-server:/root/.vscode-server ^
+-v %cd%/.docker/.vscode-server-insiders:/root/.vscode-server-insiders ^
+-w /home/workdir ^
+-p 8888:8888 ^
+rszostak/gis-jupyter
